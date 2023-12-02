ae287d99fdef145421e58dcab38669640d066b78
Syam Sundar K <k.syamsundar21@gmail.com>
2023-12-02 15:18:30 -0800

    change room primary key

--- app/src/main/java/work/syam/knockknock/data/model/UserRoom.kt
+++ app/src/main/java/work/syam/knockknock/data/model/UserRoom.kt
@@ -3,7 +3,6 @@ package work.syam.knockknock.data.model
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.UUID
 
 
 const val USER_TABLE_NAME = "USER_TABLE_ROOM"
@@ -11,16 +10,16 @@ const val USER_TABLE_NAME = "USER_TABLE_ROOM"
 @Entity(tableName = USER_TABLE_NAME)
 data class UserRoom(
     @PrimaryKey
-    @ColumnInfo(name = "uID")
-    val uID: String = UUID.randomUUID().toString(),
+//    @ColumnInfo(name = "uID")
+//    val uID: String = UUID.randomUUID().toString(),
+    @ColumnInfo(name = "id")
+    val id: Int,
     @ColumnInfo(name = "name")
     val name: String,
     @ColumnInfo(name = "avatarUrl")
     val avatarUrl: String,
     @ColumnInfo(name = "followers")
     val followers: Int,
-    @ColumnInfo(name = "id")
-    val id: Int,
     @ColumnInfo(name = "location")
     val location: String
 )
